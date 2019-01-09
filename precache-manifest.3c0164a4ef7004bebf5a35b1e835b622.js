65c1b8716a79715423342dd2bc7379234a830c5d
zmichgen <zmichgen@gmail.com>
2019-01-09 18:26:38 +0300

    Updates

---
+++ precache-manifest.3c0164a4ef7004bebf5a35b1e835b622.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e07c1bb8b2e87f53ca3d357c8dc9a77",
+    "url": "/game/static/media/tree.7e07c1bb.png"
+  },
+  {
+    "revision": "c26132ddeb6b0b2d0fdf",
+    "url": "/game/static/css/main.f376b279.chunk.css"
+  },
+  {
+    "revision": "154c2305162daa58b4fcf2fae753404c",
+    "url": "/game/static/media/footprints-fon.154c2305.jpg"
+  },
+  {
+    "revision": "e40044852da31d984f46",
+    "url": "/game/static/js/1.e4004485.chunk.js"
+  },
+  {
+    "revision": "dffb2a4fcbf3ea7182df",
+    "url": "/game/static/js/runtime~main.dffb2a4f.js"
+  },
+  {
+    "revision": "03c7e34541ac7da7b0d126c748bedea8",
+    "url": "/game/static/media/fav.03c7e345.png"
+  },
+  {
+    "revision": "c26132ddeb6b0b2d0fdf",
+    "url": "/game/static/js/main.c26132dd.chunk.js"
+  },
+  {
+    "revision": "56aa9318f47a637ca9525495fc72635a",
+    "url": "/game/static/media/button-push.56aa9318.mp3"
+  },
+  {
+    "revision": "6e1751120cb151dd93b70df6695edc68",
+    "url": "/game/static/media/sneg.6e175112.jpg"
+  },
+  {
+    "revision": "f6ec6f75655e156ff07d869c857740af",
+    "url": "/game/static/media/sany.f6ec6f75.jpg"
+  },
+  {
+    "revision": "e40044852da31d984f46",
+    "url": "/game/static/css/1.578f3731.chunk.css"
+  },
+  {
+    "revision": "e471e7c5678d2f47249a8478836e227c",
+    "url": "/game/index.html"
+  }
+];
